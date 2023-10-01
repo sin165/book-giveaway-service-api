65adb753ec2abfff3c905e69679c2a0743ae6bc3
Tamar Sinauridze <sin165165@gmail.com>
2023-10-01 19:11:51 +0400

    add documentation

--- book_giveaway_service/urls.py
+++ book_giveaway_service/urls.py
@@ -17,9 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('books.urls')),
+    path('api_schema/', get_schema_view(title='API Schema', description='Guide for the REST API'), name='api_schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='docs.html',
+        extra_context={'schema_url':'api_schema'}
+        ), name='swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
